1a8a51bb4cff74cef6ccc7b6310e88d1a8eaf70b
Waseem-Ahmed-dev <waseemahmwd51@gmail.com>
2026-03-08 16:47:25 +0500

    start

---
+++ about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import page from '../app/page'
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ contect/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import page from '../app/page'
+
+const Contect= () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+    </div>
+  )
+}
+
+export default Contect
